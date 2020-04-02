53b86dd514b544f328f453fb50b8deabfa5cc107
Alexander Verbruggen <alex@nabu.be>
2020-04-02 21:21:31 +0200

    updated to data.js

--- frappe.js
+++ frappe.js
@@ -1,264 +1,288 @@
-// check out: https://frappe.io/charts/docs
-Vue.view("frappe-chart", {
-	category: "Charts",
-	name: "Frappe Chart",
-	description: "Use this versatile frappe plugin to draw bar, line, pie and percent charts.",
-	icon: "images/components/frappe-logo.png",
-	props: {
-		page: {
-			type: Object,
-			required: true
-		},
-		parameters: {
-			type: Object,
-			required: false
-		},
-		cell: {
-			type: Object,
-			required: true
-		},
-		edit: {
-			type: Boolean,
-			required: true
-		}
-	},
-	computed: {
-		hasBar: function() {
-			return this.cell.state.datasets && this.cell.state.datasets.filter(function(x) {
-				return x.type == "bar";
-			}).length >= 1;
-		},
-		hasMultipleBars: function() {
-			return this.cell.state.datasets && this.cell.state.datasets.filter(function(x) {
-				return x.type == "bar";
-			}).length >= 2;
-		},
-		hasLines: function() {
-			return this.cell.state.datasets && this.cell.state.datasets.filter(function(x) {
-				return x.type == "line";
-			}).length >= 1;
-		},
-		type: function() {
-			return !this.cell.state.type ? "mixed" : this.cell.state.type;
-		}
-	},
-	data: function() {
-		return {
-			records: [],
-			configuring: false
-		}
-	},
-	created: function() {
-		if (!this.cell.state.bindings) {
-			Vue.set(this.cell.state, "bindings", {});
-		}
-		if (!this.cell.state.datasets) {
-			Vue.set(this.cell.state, "datasets", []);
-		}
-		if (!this.cell.state.labelFormat) {
-			Vue.set(this.cell.state, "labelFormat", {});
-		}
-	},
-	activate: function(done) {
-		this.execute().then(done, done);
-	},
-	ready: function() {
-		this.draw();
-	},
-	methods: {
-		configure: function() {
-			this.configuring = true;	
-		},
-		getKeys: function(name) {
-			var keys = this.cell.state.operation ? this.$services.page.getSimpleKeysFor({properties:this.$services.data.getDefinition(this.cell.state.operation)}) : [];
-			if (name) {
-				keys = keys.filter(function(x) { return x.toLowerCase().indexOf(name.toLowerCase()) >= 0 });
+window.addEventListener("load", function () {
+	// check out: https://frappe.io/charts/docs
+	Vue.view("frappe-chart", {
+		mixins: [nabu.page.views.data.DataCommon],
+		category: "Charts",
+		name: "Frappe Chart",
+		description: "Use this versatile frappe plugin to draw bar, line, pie and percent charts.",
+		icon: "images/components/frappe-logo.png",
+		props: {
+			page: {
+				type: Object,
+				required: true
+			},
+			parameters: {
+				type: Object,
+				required: false
+			},
+			cell: {
+				type: Object,
+				required: true
+			},
+			edit: {
+				type: Boolean,
+				required: true
 			}
-			return keys;
 		},
-		execute: function() {
-			var promise = this.$services.q.defer();
+		computed: {
+			hasBar: function() {
+				return this.cell.state.datasets && this.cell.state.datasets.filter(function(x) {
+					return x.type == "bar";
+				}).length >= 1;
+			},
+			hasMultipleBars: function() {
+				return this.cell.state.datasets && this.cell.state.datasets.filter(function(x) {
+					return x.type == "bar";
+				}).length >= 2;
+			},
+			hasLines: function() {
+				return this.cell.state.datasets && this.cell.state.datasets.filter(function(x) {
+					return x.type == "line";
+				}).length >= 1;
+			},
+			type: function() {
+				return !this.cell.state.type ? "mixed" : this.cell.state.type;
+			}
+		},
+		data: function() {
+			return {
+				configuring: false
+			}
+		},
+		beforeDestroy: function() {
+			this.$services.page.destroy(this);
+		},
+		created: function() {
+			this.create();
+			if (!this.cell.state.bindings) {
+				Vue.set(this.cell.state, "bindings", {});
+			}
+			if (!this.cell.state.datasets) {
+				Vue.set(this.cell.state, "datasets", []);
+			}
+			if (!this.cell.state.labelFormat) {
+				Vue.set(this.cell.state, "labelFormat", {});
+			}
+		},
+		activate: function(done) {
 			var self = this;
-			var pageInstance = self.$services.page.getPageInstance(self.page, self);
-			try {
-				if (this.cell.state.operation) {
-					// any input parameters
-					var parameters = {};
-					if (this.cell.bindings) {
-						Object.keys(this.cell.bindings).forEach(function(key) {
-							if (self.cell.bindings[key]) {
-								parameters[key] = self.$services.page.getBindingValue(pageInstance, self.cell.bindings[key], self);
+			this.activate(function() {
+				done();
+			}, done);
+		},
+		ready: function() {
+			this.draw();
+		},
+		methods: {
+			configure: function() {
+				this.configuring = true;	
+			},
+			getKeys: function(name) {
+				return this.keys.filter(function(x) {
+					return !name || x.toLowerCase().indexOf(name.toLowerCase()) >= 0;
+				});
+				var keys = this.cell.state.operation ? this.$services.page.getSimpleKeysFor({properties:this.$services.data.getDefinition(this.cell.state.operation)}) : [];
+				if (name) {
+					keys = keys.filter(function(x) { return x.toLowerCase().indexOf(name.toLowerCase()) >= 0 });
+				}
+				return keys;
+			},
+			execute: function() {
+				var promise = this.$services.q.defer();
+				var self = this;
+				var pageInstance = self.$services.page.getPageInstance(self.page, self);
+				try {
+					if (this.cell.state.operation) {
+						// any input parameters
+						var parameters = {};
+						if (this.cell.bindings) {
+							Object.keys(this.cell.bindings).forEach(function(key) {
+								if (self.cell.bindings[key]) {
+									parameters[key] = self.$services.page.getBindingValue(pageInstance, self.cell.bindings[key], self);
+								}
+							});
+						}
+						this.$services.data.execute(this.cell.state.operation, parameters).then(function(result) {
+							if (result) {
+								if (!(result instanceof Array)) {
+									Object.keys(result).forEach(function(x) {
+										if (result[x] instanceof Array) {
+											result = result[x];
+										}
+									});
+								}
+								if (result instanceof Array) {
+									nabu.utils.arrays.merge(self.records, result);
+									promise.resolve(result);
+								}
 							}
+						}, promise);
+					}
+					else {
+						promise.resolve();
+					}
+				}
+				catch (exception) {
+					console.error("Could not run operation", exception);
+					promise.reject();
+				}
+				return promise;
+			},
+			draw: function() {
+				var self = this;
+				if (this.records.length) {
+					var parameters = {
+						type: this.type == "mixed" ? "axis-mixed" : this.type,
+						data: {
+							datasets: []
+						},
+						colors: [],
+						tooltipOptions: {
+							formatTooltipX: function(d) { return d },
+							formatTooltipY: function(d) {
+								// we have a float, round it
+								if (typeof(d) == "number" && parseInt(d) != d) {
+									return self.$services.formatter.number(d, 2);
+								}
+								else if (typeof(d) == "string" && d.match && d.match(/^[0-9]+$/)) {
+									return parseInt(d);
+								}
+								else if (typeof(d) == "string" && d.match && d.match(/^[0-9.]+$/)) {
+									return parseFloat(d);
+								}
+								return d;
+							}
+						},
+						barOptions: {},
+						lineOptions: {},
+						axisOptions: {}
+					};
+					if (self.cell.state.title) {
+						parameters.title = this.$services.page.translate(self.cell.state.title);
+					}
+					// if we have a label field, use that
+					if (this.cell.state.label) {
+						parameters.data.labels = this.records.map(function(x) {
+							var value = self.$services.page.getValue(x, self.cell.state.label);
+							if (self.cell.state.labelFormat) {
+								value = self.$services.formatter.format(value, self.cell.state.labelFormat);
+							}
+							return value;
 						});
 					}
-					this.$services.data.execute(this.cell.state.operation, parameters).then(function(result) {
-						if (result) {
-							if (!(result instanceof Array)) {
-								Object.keys(result).forEach(function(x) {
-									if (result[x] instanceof Array) {
-										result = result[x];
-									}
-								});
+					if (this.cell.state.datasets && this.cell.state.datasets.length) {
+						var datasets = [];
+						nabu.utils.arrays.merge(datasets, this.cell.state.datasets);
+						// we want to draw bar charts first, otherwise they provide an offset for the line charts (check out frappe documentation)
+						datasets.sort(function(a, b) {
+							if (a.type == "bar" && b.type != "bar") {
+								return -1;
 							}
-							if (result instanceof Array) {
-								nabu.utils.arrays.merge(self.records, result);
-								promise.resolve(result);
+							else if (b.type == "bar" && a.type != "bar") {
+								return 1;
 							}
-						}
-					}, promise);
-				}
-				else {
-					promise.resolve();
-				}
-			}
-			catch (exception) {
-				console.error("Could not run operation", exception);
-				promise.reject();
-			}
-			return promise;
-		},
-		draw: function() {
-			var self = this;
-			if (this.records.length) {
-				var parameters = {
-					type: this.type == "mixed" ? "axis-mixed" : this.type,
-					data: {
-						datasets: []
-					},
-					colors: [],
-					tooltipOptions: {
-						formatTooltipX: function(d) { return d },
-						formatTooltipY: function(d) {
-							// we have a float, round it
-							if (typeof(d) == "number" && parseInt(d) != d) {
-								return self.$services.formatter.number(d, 2);
-							}
-							else if (typeof(d) == "string" && d.match && d.match(/^[0-9]+$/)) {
-								return parseInt(d);
-							}
-							else if (typeof(d) == "string" && d.match && d.match(/^[0-9.]+$/)) {
-								return parseFloat(d);
-							}
-							return d;
-						}
-					},
-					barOptions: {},
-					lineOptions: {},
-					axisOptions: {}
-				};
-				if (self.cell.state.title) {
-					parameters.title = this.$services.page.translate(self.cell.state.title);
-				}
-				// if we have a label field, use that
-				if (this.cell.state.label) {
-					parameters.data.labels = this.records.map(function(x) {
-						var value = self.$services.page.getValue(x, self.cell.state.label);
-						if (self.cell.state.labelFormat) {
-							value = self.$services.formatter.format(value, self.cell.state.labelFormat);
-						}
-						return value;
-					});
-				}
-				if (this.cell.state.datasets && this.cell.state.datasets.length) {
-					var datasets = [];
-					nabu.utils.arrays.merge(datasets, this.cell.state.datasets);
-					// we want to draw bar charts first, otherwise they provide an offset for the line charts (check out frappe documentation)
-					datasets.sort(function(a, b) {
-						if (a.type == "bar" && b.type != "bar") {
-							return -1;
-						}
-						else if (b.type == "bar" && a.type != "bar") {
-							return 1;
-						}
-						return 0;
-					});
-					datasets.forEach(function(x) {
-						parameters.data.datasets.push({
-							name: self.$services.page.translate(x.name),
-							chartType: x.type ? x.type : 'line',
-							values: self.records.map(function(y) {
+							return 0;
+						});
+						datasets.forEach(function(x) {
+							var hasData = false;
+							var values = self.records.map(function(y) {
 								var value = self.$services.page.getValue(y, x.value);
 								if (x.format) {
 									value = self.$services.formatter.format(value, x.format);
 								}
-								return value;
-							})
+								if (value != null) {
+									hasData = true;
+								}
+								return value == null ? 0 : value;
+							});
+							if (hasData) {
+								parameters.data.datasets.push({
+									name: self.$services.page.translate(x.name),
+									chartType: x.type ? x.type : 'line',
+									values: values
+								});
+								parameters.colors.push(x.color ? x.color : self.$services.page.getNameColor(x.name ? x.name : "unnamed" + Math.random()));
+							}
 						});
-						parameters.colors.push(x.color ? x.color : self.$services.page.getNameColor(x.name ? x.name : "unnamed" + Math.random()));
-					});
+					}
+					if (this.cell.state.stackBars) {
+						parameters.barOptions.stacked = 1;
+					}
+					if (this.cell.state.valuesOverPoints) {
+						parameters.valuesOverPoints = 1;
+					}
+					if (this.cell.state.hideDots) {
+						parameters.lineOptions.hideDots = 1;
+					}
+					if (this.cell.state.hideLine) {
+						parameters.lineOptions.hideLine = 1;
+					}
+					if (this.cell.state.regionFill) {
+						parameters.lineOptions.regionFill = 1;
+					}
+					if (this.cell.state.xIsSeries) {
+						parameters.axisOptions.xIsSeries = true;
+					}
+					if (this.cell.state.spaceRatio) {
+						parameters.barOptions.spaceRatio = parseFloat(this.cell.state.spaceRatio);
+					}
+					if (this.cell.state.dotSize) {
+						parameters.lineOptions.dotSize = parseInt(this.cell.state.dotSize);
+					}
+					if (this.cell.state.navigable) {
+						parameters.isNavigable = this.cell.state.navigable;
+					}
+					if (this.cell.state.maxSlices) {
+						parameters.maxSlices = parseInt(this.cell.state.maxSlices);
+					}
+					if (this.cell.state.height) {
+						parameters.height = parseInt(this.cell.state.height);
+					}
+					console.log("drawing", this.$refs.chart, parameters);
+					var chart = new frappe.Chart(this.$refs.chart, parameters);
+	//				chart.export();
+					// update the entire data set (only data? not settings etc)
+	//				chart.update(data);
 				}
-				if (this.cell.state.stackBars) {
-					parameters.barOptions.stacked = 1;
+			}
+		},
+		watch: {
+			'cell.state.operation': function(newValue) {
+				if (newValue) {
+					if (!this.cell.state.type) {
+						this.cell.state.type = "mixed";
+					}
+					if (!this.cell.state.label) {
+						var self = this;
+						var definition = this.$services.data.getDefinition(this.cell.state.operation);
+						Object.keys(definition).forEach(function(key) {
+							if (!self.cell.state.label && (definition[key].type == "string" || !definition[key].type)) {
+								self.cell.state.label = key;
+							}
+						});
+					}
+					if (!this.cell.state.datasets.length) {
+						var self = this;
+						var definition = this.$services.data.getDefinition(this.cell.state.operation);
+						Object.keys(definition).forEach(function(key) {
+							if (definition[key].type == "number" || definition[key].format == "int32" || definition[key].format == "int64" || !definition[key].type) {
+								var dataset = {
+									type: "line",
+									valueFormat: {}
+								};
+								dataset.value = key;
+								dataset.name = key;
+								self.cell.state.datasets.push(dataset);
+							}
+						});
+					}
+					this.execute().then(this.draw);
 				}
-				if (this.cell.state.valuesOverPoints) {
-					parameters.valuesOverPoints = 1;
-				}
-				if (this.cell.state.hideDots) {
-					parameters.lineOptions.hideDots = 1;
-				}
-				if (this.cell.state.hideLine) {
-					parameters.lineOptions.hideLine = 1;
-				}
-				if (this.cell.state.regionFill) {
-					parameters.lineOptions.regionFill = 1;
-				}
-				if (this.cell.state.xIsSeries) {
-					parameters.axisOptions.xIsSeries = true;
-				}
-				if (this.cell.state.spaceRatio) {
-					parameters.barOptions.spaceRatio = parseFloat(this.cell.state.spaceRatio);
-				}
-				if (this.cell.state.dotSize) {
-					parameters.lineOptions.dotSize = parseInt(this.cell.state.dotSize);
-				}
-				if (this.cell.state.navigable) {
-					parameters.isNavigable = this.cell.state.navigable;
-				}
-				if (this.cell.state.maxSlices) {
-					parameters.maxSlices = parseInt(this.cell.state.maxSlices);
-				}
-				if (this.cell.state.height) {
-					parameters.height = parseInt(this.cell.state.height);
-				}
-				var chart = new frappe.Chart(this.$el, parameters);
-//				chart.export();
-				// update the entire data set (only data? not settings etc)
-//				chart.update(data);
 			}
 		}
-	},
-	watch: {
-		'cell.state.operation': function(newValue) {
-			if (newValue) {
-				if (!this.cell.state.type) {
-					this.cell.state.type = "mixed";
-				}
-				if (!this.cell.state.label) {
-					var self = this;
-					var definition = this.$services.data.getDefinition(this.cell.state.operation);
-					Object.keys(definition).forEach(function(key) {
-						if (!self.cell.state.label && (definition[key].type == "string" || !definition[key].type)) {
-							self.cell.state.label = key;
-						}
-					});
-				}
-				if (!this.cell.state.datasets.length) {
-					var self = this;
-					var definition = this.$services.data.getDefinition(this.cell.state.operation);
-					Object.keys(definition).forEach(function(key) {
-						if (definition[key].type == "number" || definition[key].format == "int32" || definition[key].format == "int64" || !definition[key].type) {
-							var dataset = {
-								type: "line",
-								valueFormat: {}
-							};
-							dataset.value = key;
-							dataset.name = key;
-							self.cell.state.datasets.push(dataset);
-						}
-					});
-				}
-				this.execute().then(this.draw);
-			}
-		}
-	}
+	});
 });
+
+
+
+
--- frappe.tpl
+++ frappe.tpl
@@ -1,53 +1,61 @@
 <template id="frappe-chart">
-	<div class="frappe-chart"><n-sidebar class="settings" v-if="configuring == true" :inline="true" @close="configuring = false; draw()">
-		<n-collapsible title="Chart Data" class="padded">
-			<h2>Data<span class="subscript">Determine where you will get your data from</span></h2>
-			<n-form-combo label="Operation" v-model="cell.state.operation" 
-				:formatter="function(x) { return x.id }"
-				:extracter="function(x) { return x.id }"
-				:filter="$services.data.getDataOperations"/>
-			<n-page-mapper v-if="cell.state.operation" :to="$services.data.getInputParameters(cell.state.operation)" 
-				:from="$services.page.getAvailableParameters(page, cell, true)" 
-				v-model="cell.bindings"/>
-			<n-form-combo v-model="cell.state.type" label="Chart Type" info="The type of chart you want to create. If you want to create a line and/or bar chart, use the mixed option" :items="['mixed', 'pie', 'percentage']" @input="draw"/>
-			<n-form-text v-model="cell.state.height" label="Chart height (in px)" info="The default height is 240" @input="draw"/>
-		</n-collapsible>
-		<n-collapsible title="Chart Content" v-if="cell.state.operation" class="padded">
-			<n-form-combo v-model="cell.state.label" label="Label Field" :filter="getKeys" @input="draw"/>
-			<page-formatted-configure v-if="cell.state.label" :fragment="cell.state.labelFormat" :page="page" :cell="cell" @input="draw"/>
-			
-			<n-form-text v-model="cell.state.maxSlices" v-if="type == 'pie'" label="Max amount of slices" @input="draw"/>
-			
-			<n-form-switch v-if="type == 'mixed'" v-model="cell.state.xIsSeries" label="Treat X-axis as series" info="When there are too many labels to show correctly and the x-axis is considered a continuous series, some labels can be left out" @input="draw"/>
-			<n-form-switch v-if="type == 'mixed'" v-model="cell.state.valuesOverPoints" label="Show values over points" @input="draw"/>
-			<n-form-switch v-model="cell.state.navigable" label="Navigable" @input="draw"/>
-			<n-form-switch v-model="cell.state.hideDots" v-if="hasLines && !cell.state.hideLine" label="Hide line dots" @input="draw"/>
-			<n-form-switch v-model="cell.state.hideLine" v-if="hasLines && !cell.state.hideDots" label="Hide line" @input="draw"/>
-			<n-form-text type="range" :minimum="3" :maximum="10" :step="1" v-model="cell.state.dotSize" v-if="hasLines && !cell.state.hideDots" label="Dot size" @input="draw"/>
-			<n-form-switch v-model="cell.state.regionFill" v-if="hasLines" label="Fill region" @input="draw"/>
-			<n-form-switch v-model="cell.state.stackBars" v-if="hasMultipleBars" label="Stack bars" @input="draw"/>
-			<n-form-text type="range" :minimum="0.1" :maximum="1.9" :step="0.1" v-model="cell.state.spaceRatio" v-if="hasBar" label="Bar chart space ratio" info="The ratio of the space between the bars to the width of the bars themselves" @input="draw"/>
-			<h2>Data Sets<span class="subscript">Choose the fields you want to use</span></h2>
-			<div class="list-actions">
-				<button @click="cell.state.datasets.push({valueFormat: {}})"><span class="fa fa-plus"></span>Data set</button>
-			</div>
-			<div v-for="i in Object.keys(cell.state.datasets)" class="list-row">
-				<n-form-text v-model="cell.state.datasets[i].name" label="Name" @input="draw"/>
-				<n-form-combo v-if="type == 'mixed'" v-model="cell.state.datasets[i].type" :items="['line', 'bar']" label="Type" @input="draw"/>
-				<n-form-text v-model="cell.state.datasets[i].color" label="Color" type="color" @input="draw"/>
-				<n-form-combo v-model="cell.state.datasets[i].value" label="Value Field" 
-					:filter="getKeys" @input="draw"/>
-				<page-formatted-configure v-if="cell.state.datasets[i].value" :fragment="cell.state.datasets[i].valueFormat" :page="page" :cell="cell" @input="draw"/>
-				<span class="fa fa-times" @click="cell.state.datasets.splice(i, 1); draw()"></span>
-			</div>
-		</n-collapsible>
-	</n-sidebar><div class="page-startup-wizard" v-if="edit && !cell.state.operation">
-			<div class="step">
-				<h2 class="title">Choose a data source</h2>
-				<n-form-combo label="Operation" v-model="cell.state.operation" 
-					:formatter="function(x) { return x.id }"
-					:extracter="function(x) { return x.id }"
-					:filter="$services.data.getDataOperations"/>
-			</div>
-		</div></div>
+	<div class="frappe-chart">
+		
+		<data-common-header :page="page" :parameters="parameters" :cell="cell"
+			:edit="edit"
+			:records="records"
+			:selected="selected"
+			:inactive="inactive"
+			@updatedEvents="$emit('updatedEvents')"
+			@close="$emit('close'); configuring=false"
+			:multiselect="true"
+			:configuring="configuring"
+			:updatable="true"
+			:paging="paging"
+			:filters="filters"
+			@refresh="refresh">
+
+			<n-collapsible slot="settings" title="Chart Content" class="padded">
+				<n-form-combo v-model="cell.state.label" label="Label Field" :filter="getKeys" @input="draw"/>
+				<page-formatted-configure v-if="cell.state.label" :fragment="cell.state.labelFormat" :page="page" :cell="cell" @input="draw"/>
+				
+				<n-form-text v-model="cell.state.maxSlices" v-if="type == 'pie'" label="Max amount of slices" @input="draw"/>
+				
+				<n-form-switch v-if="type == 'mixed'" v-model="cell.state.xIsSeries" label="Treat X-axis as series" info="When there are too many labels to show correctly and the x-axis is considered a continuous series, some labels can be left out" @input="draw"/>
+				<n-form-switch v-if="type == 'mixed'" v-model="cell.state.valuesOverPoints" label="Show values over points" @input="draw"/>
+				<n-form-switch v-model="cell.state.navigable" label="Navigable" @input="draw"/>
+				<n-form-switch v-model="cell.state.hideDots" v-if="hasLines && !cell.state.hideLine" label="Hide line dots" @input="draw"/>
+				<n-form-switch v-model="cell.state.hideLine" v-if="hasLines && !cell.state.hideDots" label="Hide line" @input="draw"/>
+				<n-form-text type="range" :minimum="3" :maximum="10" :step="1" v-model="cell.state.dotSize" v-if="hasLines && !cell.state.hideDots" label="Dot size" @input="draw"/>
+				<n-form-switch v-model="cell.state.regionFill" v-if="hasLines" label="Fill region" @input="draw"/>
+				<n-form-switch v-model="cell.state.stackBars" v-if="hasMultipleBars" label="Stack bars" @input="draw"/>
+				<n-form-text type="range" :minimum="0.1" :maximum="1.9" :step="0.1" v-model="cell.state.spaceRatio" v-if="hasBar" label="Bar chart space ratio" info="The ratio of the space between the bars to the width of the bars themselves" @input="draw"/>
+				<h2>Data Sets<span class="subscript">Choose the fields you want to use</span></h2>
+				<div class="list-actions">
+					<button @click="cell.state.datasets.push({valueFormat: {}})"><span class="fa fa-plus"></span>Data set</button>
+				</div>
+				<div v-for="i in Object.keys(cell.state.datasets)" class="list-row">
+					<n-form-text v-model="cell.state.datasets[i].name" label="Name" @input="draw"/>
+					<n-form-combo v-if="type == 'mixed'" v-model="cell.state.datasets[i].type" :items="['line', 'bar']" label="Type" @input="draw"/>
+					<n-form-text v-model="cell.state.datasets[i].color" label="Color" type="color" @input="draw"/>
+					<n-form-combo v-model="cell.state.datasets[i].value" label="Value Field" 
+						:filter="getKeys" @input="draw"/>
+					<page-formatted-configure v-if="cell.state.datasets[i].value" :fragment="cell.state.datasets[i].valueFormat" :page="page" :cell="cell" @input="draw"/>
+					<span class="fa fa-times" @click="cell.state.datasets.splice(i, 1); draw()"></span>
+				</div>
+			</n-collapsible>
+		</data-common-header>
+		<div ref="chart"></div>
+		<data-common-footer :page="page" :parameters="parameters" :cell="cell" 
+			:edit="edit"
+			:records="records"
+			:selected="selected"
+			:inactive="inactive"
+			:global-actions="globalActions"
+			@updatedEvents="$emit('updatedEvents')"
+			@close="$emit('close')"
+			:multiselect="true"
+			:updatable="true"/>
+	</div>
 </template>
+
